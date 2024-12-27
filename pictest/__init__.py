b7a0ecbbad556ee4e86e5dfd233a16f74d6a15db
Felix Soubelet <felix.soubelet@protonmail.com>
2024-12-27 17:49:53 +0100

    init

--- pictest/__init__.py
+++ pictest/__init__.py
@@ -1,3 +1,13 @@
-from ._meshgrid import MeshGrid
+from ._meshgrid import MeshGrid, create_mesh
 from ._pic import IBSParticleInCell
+from ._plotting import plot_meshgrid_points, plot_meshgrid_voxels
 from ._setup import install_ibs_pic
+
+__all__ = [
+    "MeshGrid",
+    "create_mesh",
+    "IBSParticleInCell",
+    "install_ibs_pic",
+    "plot_meshgrid_points",
+    "plot_meshgrid_voxels",
+]
