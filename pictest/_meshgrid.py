0e7beeaaa96be22929cd2e3cb76f180a16ff1706
Felix Soubelet <felix.soubelet@protonmail.com>
2024-12-27 17:41:11 +0100

    unused imports

--- pictest/_meshgrid.py
+++ pictest/_meshgrid.py
@@ -7,7 +7,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import numpy as np
-import xobjects as xo
 import xtrack as xt
 
 from xfields.fieldmaps.interpolated import _configure_grid
