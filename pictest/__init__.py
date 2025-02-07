9525b21ddb3965d9a0f82febcbe20744ef566fc4
Felix Soubelet <felix.soubelet@protonmail.com>
2025-02-07 09:51:06 +0100

    useful context manager for tests

--- pictest/__init__.py
+++ pictest/__init__.py
@@ -1,3 +1,6 @@
+import contextlib
+import sys
+
 from ._meshgrid import MeshGrid, create_mesh
 from ._pic import IBSParticleInCell
 from ._plotting import plot_meshgrid_points, plot_meshgrid_voxels, plot_particles_3dtrace
@@ -5,11 +8,26 @@ from ._setup import install_ibs_pic
 
 __version__ = "0.1.0"
 
+
+class _DummyFile(object):
+    def write(self, x):
+        pass
+
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = _DummyFile()
+    yield
+    sys.stdout = save_stdout
+
+
 __all__ = [
     "MeshGrid",
     "create_mesh",
     "IBSParticleInCell",
     "install_ibs_pic",
+    "nostdout",
     "plot_meshgrid_points",
     "plot_meshgrid_voxels",
     "plot_particles_3dtrace",
